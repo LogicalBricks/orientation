adc852b4ee22c9bc7b57018a81d68e201f9a61ed
thotmx <hermes.o.r@gmail.com>
2015-05-15 17:11:16 -0500

    Changing the host information

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, :production
 
-role :app, %w{deployerdemo@demo-infacto.com}
-role :web, %w{deployerdemo@demo-infacto.com}
-role :db,  %w{deployerdemo@demo-infacto.com}
+role :app, %w{deployerdemo@knowledge.logicalbricks.com}
+role :web, %w{deployerdemo@knowledge.logicalbricks.com}
+role :db,  %w{deployerdemo@knowledge.logicalbricks.com}
 
 server 'demo-infacto.com', user: 'deployerdemo', roles: %w{web app db}
 
